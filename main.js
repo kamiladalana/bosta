d043432ec99bc43bd42b75fa05e6f1e147f90752
kamiladalana <143311706+kamiladalana@users.noreply.github.com>
2024-09-23 11:49:24 -0300

    ew

--- main.js
+++ main.js
@@ -4,14 +4,13 @@ function soma (num1, num2){
 return num1+num2}
 console.log(soma(4,2));
 
-function areaQuadrado
-lado {
-    return lado**2,
+function areaQuadrado (lado){
+    return lado**2;
 }
 console.log (areaQuadrado)
 
 let areaTriangulo =
 function (        ) {
-    return  ,
+    return c2=a2+b2,
 }
 console.log ()
