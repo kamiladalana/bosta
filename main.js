bc5aa22da8d690c3d5143dd89f90b176c9949395
kamiladalana <143311706+kamiladalana@users.noreply.github.com>
2024-09-23 11:36:27 -0300

    ☯

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ let nomeUsuario = 'Kamila';
 function soma (num1, num2){
 return num1+num2}
 console.log(soma(4,2));
+
+function soma (num1, num2){
+    return num1*num2}
+    console.log(soma(4,2));
