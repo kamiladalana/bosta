70609d9da4772f3b2baca48924c50f748ba592a7
kamiladalana <143311706+kamiladalana@users.noreply.github.com>
2024-09-30 11:35:19 -0300

    j

--- main.js
+++ main.js
@@ -28,4 +28,4 @@ function media (num3, num4, num5){
     let media = parseInt (prompt ('Qual a média dos valores?'));
     return (num3+num4+num5)/3;{}console.log (media (2,4,6));
     
-    let estados=[Acre, Alagoas, Amapá, Amazonas, Bahia, Ceará, Espírito Santo, Goiás, Maranhão,Mato Grosso, Mato Grosso do Sul, Minas Gerais, Pará, Paraíba, Paraná, Pernambuco, Piauí, Rio de Janeiro, Rio Grande do Norte, Rio Grande do Sul, Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo, Sergipe', 'Tocantins','DistritoFederal']
+    let estados=['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão','Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins','DistritoFederal']
