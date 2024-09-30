a8bd94dbbba13e4cb199a593b24f898e9901b88b
kamiladalana <143311706+kamiladalana@users.noreply.github.com>
2024-09-30 11:36:52 -0300

    a

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
-let nomeUsuario = 'Kamila';
-// alert (nomeUsuario)
-function soma (num1, num2){
-return num1+num2}
-console.log(soma(4,2));
+// let nomeUsuario = 'Kamila';
+// // alert (nomeUsuario)
+// function soma (num1, num2){
+// return num1+num2}
+// console.log(soma(4,2));
 
-function areaQuadrado (lado){
-    return lado**2;
-}
-console.log (areaQuadrado)
+// function areaQuadrado (lado){
+//     return lado**2;
+// }
+// console.log (areaQuadrado)
 
-let areaTriangulo =
-function (base, altura) {
-    return (base*altura)/2;
-}
-console.log (areaTriangulo)
+// let areaTriangulo =
+// function (base, altura) {
+//     return (base*altura)/2;
+// }
+// console.log (areaTriangulo)
 
-let a = parseInt(prompt ("primeiro valor"))
-let b = parseInt(prompt ("segundo valor"))
-let c = parseInt(prompt ("terceiro valor"))
+// let a = parseInt(prompt ("primeiro valor"))
+// let b = parseInt(prompt ("segundo valor"))
+// let c = parseInt(prompt ("terceiro valor"))
 
-function media(a,b,c) {
-    return((a+b+c)/3);
-}
-document.write(media(a, b, c))
+// function media(a,b,c) {
+//     return((a+b+c)/3);
+// }
+// document.write(media(a, b, c))
 
-function media (num3, num4, num5){
-    let media = parseInt (prompt ('Qual a média dos valores?'));
-    return (num3+num4+num5)/3;{}console.log (media (2,4,6));
+// function media (num3, num4, num5){
+//     let media = parseInt (prompt ('Qual a média dos valores?'));
+//     return (num3+num4+num5)/3;{}console.log (media (2,4,6));
     
-    let estados=['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão','Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins','DistritoFederal']
+    let estados=['Acre', 'Alagoas', 'Amapá', 'Amazonas', 'Bahia', 'Ceará', 'Espírito Santo', 'Goiás', 'Maranhão','Mato Grosso', 'Mato Grosso do Sul', 'Minas Gerais', 'Pará', 'Paraíba', 'Paraná', 'Pernambuco', 'Piauí', 'Rio de Janeiro', 'Rio Grande do Norte', 'Rio Grande do Sul', 'Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo', 'Sergipe', 'Tocantins','DistritoFederal']estados
