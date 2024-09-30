34cc2ef55b5401fdc00f879adf22922189c65e31
kamiladalana <143311706+kamiladalana@users.noreply.github.com>
2024-09-30 11:30:59 -0300

    l

--- main.js
+++ main.js
@@ -23,3 +23,9 @@ function media(a,b,c) {
     return((a+b+c)/3);
 }
 document.write(media(a, b, c))
+
+function media (num3, num4, num5){
+    let media = parseInt (prompt ('Qual a média dos valores?'));
+    return (num3+num4+num5)/3;{}console.log (media (2,4,6));
+    
+    let estados=[Acre, Alagoas, Amapá, Amazonas, Bahia, Ceará, Espírito Santo, Goiás, Maranhão,Mato Grosso, Mato Grosso do Sul, Minas Gerais, Pará, Paraíba, Paraná, Pernambuco, Piauí, Rio de Janeiro, Rio Grande do Norte, Rio Grande do Sul, Rondônia', 'Roraima', 'Santa Catarina', 'São Paulo, Sergipe', 'Tocantins','DistritoFederal']
